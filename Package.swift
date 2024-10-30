e5ba34ee6882273343403160a471863a581905b9
Andrei Shender <andrei.s@canva.com>
2024-10-30 19:31:54 +1100

    rem

--- Package.swift
+++ Package.swift
@@ -36,7 +36,6 @@ let package = Package(
         .package(url: "https://github.com/JohnSundell/CollectionConcurrencyKit.git", from: "0.2.0"),
         .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", "1.8.0"..."2.0.0"),
         .package(url: "https://github.com/ReactiveCocoa/ReactiveSwift.git", exact:"6.7.0"),
-        .package(url: "https://github.com/sensorsdata/sf-sdk-ios", "0.5.5" ..< "1000.0.0"),
         .package(url: "https://github.com/braze-inc/braze-swift-sdk", "9.0.0" ..< "1000.0.0"),
     ],
     targets: [
